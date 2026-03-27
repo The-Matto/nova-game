087eab670136c0f6f4f5f32e680c630c9f393b3f
Matthew Heritage <destroyer273@hotmail.co.uk>
2026-03-27 19:13:44 +0000

    Add nove-game-server files

---
+++ nove-game-server/Sockets.ts
@@ -0,0 +1,52 @@
+﻿import { WebSocketServer } from 'ws';
+import {parse} from 'url'
+
+export const clientsConnections = new Map<string, WebSocket>();
+
+export const CreateSocketListener = () => {
+    const wss = new WebSocketServer({ port: 8080 });
+    wss.binaryType = 'arraybuffer';
+    let a = new WebSocket("ws://localhost:8080");
+    clientsConnections.set("playerID", a);
+
+    console.log('Start listening on port: ' + wss.address().port);
+    wss.on('connection', (socket: WebSocket, req: { url: string; }) => {
+        console.log('Client connected');
+        const { query } = parse(req.url!, true);
+        const playerID = query['UID'] as string;
+        console.log(playerID);
+
+        clientsConnections.set(playerID, socket);
+
+        socket.onmessage = async event => {
+            let buffer;
+            console.log(Object.prototype.toString.call(event.data));
+            if (event.data instanceof Blob) {
+                buffer = await event.data.arrayBuffer();
+            } else if (event.data instanceof ArrayBuffer) {
+                buffer = event.data;
+            } else {
+                //console.log('Received text:', event.data);
+                return;
+            }
+
+            const view = new Uint8Array(buffer);
+            //console.log('Raw bytes:', view);
+
+            const decoder = new TextDecoder('utf-8');
+            const text = decoder.decode(view);
+            console.log('Decoded text:', text);
+        };
+
+        socket.onclose = async event => {
+        console.log('Client disconnected');
+        for (const [key, value] of clientsConnections) {
+            if (value === socket) {
+                clientsConnections.delete(key);
+                break;
+            }
+        }
+        };
+
+    });
+}
---
+++ nove-game-server/server.ts
@@ -0,0 +1,24 @@
+﻿import {clientsConnections, CreateSocketListener} from "./Sockets";
+
+
+
+
+console.log("Launching Server");
+const socket = CreateSocketListener();
+
+
+//Setup game loop
+const TICK_RATE = 60;
+const TICK_INTERVAL = 1000 / TICK_RATE;
+let testIncrement : number = 0;
+setInterval(() => {
+    const now = Date.now();
+    //console.log("Tick")
+
+    clientsConnections.forEach(async client => {
+        //client.send(testIncrement.toString() )
+        testIncrement++;
+    })
+
+}, TICK_INTERVAL);
+
